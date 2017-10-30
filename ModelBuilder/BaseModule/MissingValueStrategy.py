3d0d341be7ceb181cbc94a44211e258c716c7ca6
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-30 15:29:20 +0800

    modify default value setting

--- ModelBuilder/BaseModule/MissingValueStrategy.py
+++ ModelBuilder/BaseModule/MissingValueStrategy.py
@@ -3,15 +3,15 @@
 from sklearn.preprocessing import Imputer
 import traceback
 
+
 def fill_strategy(raw_data, strategy):
-    res = []
     try:
         if isinstance(strategy, str):
             impute = Imputer(strategy=strategy)
         else:
             impute = Imputer(missing_values=strategy)
-        res.append(impute.fit_transform(raw_data))
+        new_data = impute.fit_transform(raw_data)
     except ValueError:
         traceback.print_exc()
         raise ValueError
-    return res
+    return new_data
