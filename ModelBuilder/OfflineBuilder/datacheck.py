fe0ebcedde14301a2cff3f2055143ff453a7bc20
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-11-02 15:29:57 +0800

    modify default value setting

--- ModelBuilder/OfflineBuilder/datacheck.py
+++ ModelBuilder/OfflineBuilder/datacheck.py
@@ -4,60 +4,18 @@ import sys
 from pandas import DataFrame
 sys.path.append('../BaseModule')
 sys.path.append("../../DataReceiver")
+sys.path.append("../../Base")
 import PreProcessing as pp
 import LocalReceiver as lr
 import MongoReceiver
 import DataSampling as ds
 import traceback
-
-
-data_src = 'local'
-data_path = "../../data/fangdichan.csv"
-
-delim = ";"
-target_fields = ['wcType', 'unixTime', 'networkMode', 'area', 'adxCode', 'gender', 'price', 'appName', 'mobileOs', 'ip',
-                 'wcAcId', 'wcOrgBidPrice', 'appMediaCat', 'clicked']
-dum_coding_fields = ["unixTime", "networkMode", "mobileOs", "appMediaCat"]
-
-
-def get_origin_data(data_src, data_path, delim, target_fields):
-    train_data = {}
-    test_data = []
-    threshold = 1
-    try:
-        if data_src == "local":
-
-            # dic_exp, dic_clc = lr.GetOriginData2(data_path, delim, target_fields)  # get original data
-            dic_exp, dic_clc = lr.GetOriginData2(data_path, delim)  # get original data
-
-            dic_exp['unixTime'] = pp.Unixtime2Hour(dic_exp['unixTime'])
-            dic_clc['unixTime'] = pp.Unixtime2Hour(dic_clc['unixTime'])
-
-            print('exposure: ', len(dic_exp['unixTime']), '*'*30)
-            for k, v in dic_exp.items():
-                if len(set(v)) > threshold:
-                    print('%s: ' % k, len(set(v)))
-
-            print('click: ', len(dic_clc['unixTime']), '*'*30)
-            for k, v in dic_clc.items():
-                if len(set(v)) > threshold:
-                    print('%s: ' % k, len(set(v)))
-
-            dic_merge = ds.merge_data(dic_exp, dic_clc, 'up')
-            print('merged: ', len(dic_merge['unixTime']), '*'*30)
-            for k, v in dic_merge.items():
-                if len(set(v)) > threshold:
-                    print('%s: ' % k, len(set(v)))
-
-        elif data_src == "mongo":
-            mr = MongoReceiver.Receiver(10001)
-            train_data = mr.receiver('pCTR', 'wuhu', target_fields)
-    except:
-        traceback.print_exc()
-        pass
-    return train_data, test_data
+from read_cnf import get_conf_info
+from pandas import DataFrame
 
 
 if __name__ == '__main__':
 
-    train_data, test_data_list = get_origin_data('local', data_path, delim, target_fields)
+    conf_info = get_conf_info()
+    raw_data = lr.load_local_data(conf_info["raw_data"])  # get original data
+    print(DataFrame(raw_data))
