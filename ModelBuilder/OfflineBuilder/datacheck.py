aac3466950d8c8ef03ab9f60357e1b6be5feff5c
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-11-02 15:46:43 +0800

    modify default value setting

--- ModelBuilder/OfflineBuilder/datacheck.py
+++ ModelBuilder/OfflineBuilder/datacheck.py
@@ -11,10 +11,23 @@ import LocalReceiver as lr
 # import traceback
 from read_cnf import get_conf_info
 from pandas import DataFrame
-
+import numpy as np
+from collections import Counter
 
 if __name__ == '__main__':
 
     conf_info = get_conf_info()
     raw_data = lr.load_local_data(conf_info["raw_data"])  # get original data
     print(DataFrame(raw_data))
+
+    percentile = []
+    for idx in range(raw_data.shape[-1]):
+        column = raw_data[:, idx]
+        percentile.append(1-sum(np.isnan(column))/float(len(column)))
+
+    print(percentile)
+
+    percentile.sort()
+
+    print(percentile)
+
