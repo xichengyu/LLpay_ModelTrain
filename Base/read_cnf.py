31bb9077987a4a38200efedcacea3997b0cf15ee
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-11-16 17:39:34 +0800

    modify read_cnf.py

--- Base/read_cnf.py
+++ Base/read_cnf.py
@@ -4,15 +4,15 @@
 def get_conf_info(conf_path="../../conf/cnf.txt"):
     conf_dict = {}
     try:
-        for line in open(conf_path):
+        for line in open(conf_path).readlines():
             if line[0] == "#" or line.strip() == "":
                 continue
             line = line.strip().split(" = ")
-            conf_dict[line[0]] = line[1]
+            conf_dict[line[0]] = " = ".join(line[1:])
     except UserWarning:
         print("target conf file not exists!")
     return conf_dict
 
 
 if __name__ == "__main__":
-    print(get_conf_info()["column_name_path"])
+    print(get_conf_info())
