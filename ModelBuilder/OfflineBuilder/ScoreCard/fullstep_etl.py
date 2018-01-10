18cf552b9998034aea8b19b652f13eb2efa8f3b4
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2018-01-10 10:59:56 +0800

    modify fullstep_etl.py

--- ModelBuilder/OfflineBuilder/ScoreCard/fullstep_etl.py
+++ ModelBuilder/OfflineBuilder/ScoreCard/fullstep_etl.py
@@ -1,3 +1,21 @@
 # coding=utf-8
 
+import sys
+sys.path.append("../../BaseModule")
+# from datetime import datetime
+# import pandas as pd
+import numpy as np
+from sklearn.externals import joblib
+import MissingValueStrategy as mvs
 
+
+if __name__ == "__main__":
+    data = joblib.load("../../../data/data_xicy_fullstep_202.dt")
+    data = np.array(data)
+
+    strategies = ["mean", "median", "most_frequent"]  # different strategies for dealing with missing value
+
+    new_data = mvs.fill_strategy(data, strategy="median", missing_values=-1.0)
+
+    print(new_data.shape)
+    joblib.dump(new_data, "../../../data/data_xicy_fullstep_202_fill.dt")
